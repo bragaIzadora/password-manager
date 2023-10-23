b886ce6ddf00ff07e5335ca7b3445af4495cb5bb
Izadora Braga <izadora_abraga@outlook.com>
2023-10-23 14:38:25 -0300

    requisito 2

---
+++ src/components/Form.tsx
@@ -0,0 +1,43 @@
+function Form() {
+  return (
+    <div>
+      <div>
+        <label htmlFor="serviceName">
+          Nome do serviço
+          <input type="text" id="serviceName" name="serviceName" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="login">
+          Login
+          <input type="text" id="login" name="login" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Senha
+          <input type="password" name="password" id="password" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="url">
+          URL
+          <input type="text" id="url" name="url" />
+        </label>
+      </div>
+      <div>
+        <button>
+          Cadastrar
+        </button>
+      </div>
+      <div>
+        <button>
+          Cancelar
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Form;
